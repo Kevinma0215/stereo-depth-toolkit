d79b3a4a0bd8769e253a81ca49f77152ebb96888
Kevin Ma <kevinma20000215@gmail.com>
2026-03-10 09:57:45 -0700

    update python setup

--- src/stereo_depth/__init__.py
+++ src/stereo_depth/__init__.py
@@ -1,1 +1,45 @@
 __version__ = "0.1.0"
+
+# Entities
+from stereo_depth.entities import (
+    FramePair,
+    RectifiedPair,
+    CalibrationResult,
+    DepthMap,
+    PointCloud,
+)
+
+# Use-case ports + pipeline
+from stereo_depth.use_cases import (
+    ICameraSource,
+    ICalibrationRepo,
+    IRectifier,
+    IDisparityMatcher,
+    IDepthEstimator,
+    StereoPipeline,
+)
+
+# Calibration evaluation
+from stereo_depth.adapters.calibration import (
+    CalibrationEvaluation,
+    evaluate_calibration,
+)
+
+__all__ = [
+    # entities
+    "FramePair",
+    "RectifiedPair",
+    "CalibrationResult",
+    "DepthMap",
+    "PointCloud",
+    # use cases
+    "ICameraSource",
+    "ICalibrationRepo",
+    "IRectifier",
+    "IDisparityMatcher",
+    "IDepthEstimator",
+    "StereoPipeline",
+    # calibration evaluation
+    "CalibrationEvaluation",
+    "evaluate_calibration",
+]
